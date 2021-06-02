9fc2d0062fd307f29aeec3f203719d6944c327da
Harshit05Garg <harshit05garg@gmail.com>
2021-06-03 02:23:04 +0530

    Created Matrix Multiplication

---
+++ Problem Programs/Matrix_multiplication.c
@@ -0,0 +1,48 @@
+// This code only works for square matrix.
+#include <stdio.h>
+#define row 4
+#define column 4
+int main()
+{
+    int arr[3][row][column] = {
+        {
+            {1,2,3,1},
+            {1,2,1,1},
+            {3,1,2,1},
+            {1,1,1,1}
+        },
+        {
+            {1,2,3,1},
+            {1,2,1,1},
+            {3,1,2,1},
+            {1,1,1,1}
+        }
+    };
+    int i ,j, k, a = 0, b = 0, loop = 0;
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<column; j++)
+        {
+            int result = 0;
+            for(k=0; k<row; k++)
+            {
+                result = result + (arr[0][i][k] * arr[1][k][j]);
+            }
+            arr[2][a][b] = result;
+            printf("%d ", arr[2][a][b]);
+            b++;
+            if(b == column && a < row-1)
+            {
+                b = 0;
+                a++;
+            }
+            loop++;
+            if(loop == column)
+            {
+                printf("\n");
+                loop = 0;
+            }
+        }
+    }
+    return 0;
+}
